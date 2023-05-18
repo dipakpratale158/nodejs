f70845f440dc5d8a41f97448407622f10fa6f576
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-18 14:25:03 +0530

    rwquest and response

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain",
-  });
-  res.end("My name is Bard.");
+  const url = req.url;
+
+  switch (url) {
+    case "/":
+      res.writeHead(200, {
+        "Content-Type": "text/plain",
+      });
+      res.end("Welcome home.");
+      break;
+    case "/about":
+      res.writeHead(200, {
+        "Content-Type": "text/plain",
+      });
+      res.end("Welcome to About Us page.");
+      break;
+    case "/node":
+      res.writeHead(200, {
+        "Content-Type": "text/plain",
+      });
+      res.end("Welcome to my Node Js project.");
+      break;
+    default:
+      res.writeHead(404, {
+        "Content-Type": "text/plain",
+      });
+      res.end("Page not found.");
+      break;
+  }
 });
 
 server.listen(4000, () => {
