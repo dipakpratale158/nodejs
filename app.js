3abe8ce9370fe6ea04fd2061565bfc25d9105b42
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-19 11:35:30 +0530

    Writing and reading from a file

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
-const http = require("http");
+const fs=require("fs")
+const path=require("path")
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  switch (url) {
-    case "/":
-      res.writeHead(200, {
-        "Content-Type": "text/plain",
-      });
-      res.end("Welcome home.");
-      break;
-    case "/about":
-      res.writeHead(200, {
-        "Content-Type": "text/plain",
-      });
-      res.end("Welcome to About Us page.");
-      break;
-    case "/node":
-      res.writeHead(200, {
-        "Content-Type": "text/plain",
-      });
-      res.end("Welcome to my Node Js project.");
-      break;
-    default:
-      res.writeHead(404, {
-        "Content-Type": "text/plain",
-      });
-      res.end("Page not found.");
-      break;
+const requesthandeler=(req,res)=>{
+const url=req.url;
+const method=req.method;
+const body=[];
+if(url==="/"){
+fs.readFile("message.txt",{encoding:"utf-8"},(err,data)=>{
+  if(err){
+console.log(err)
   }
-});
+  console.log(data)
+  res.write("<html>")
+  res.write("<head><title>enter message</title></head>")
+  res.write("<head><title>enter message</title></head>")
+res.write(`<body>${data}</body>`)
+res.write(`<body><form action="/message" method="post"><input type="text" name="message" > </form></body>`)
+res.write("</html>")
+return res.end()
+})
 
-server.listen(4000, () => {
-  console.log("Server is running on http://localhost:4000");
-});
+} else if(url === "/message" && method === "post"){
+  req.on("dta",(chunk)=>{
+    body.push(chunk)
+  })
+
+  return req.on("end",()=>{
+    const parsebody=Buffer.concat(body).toString()
+    console.log(parsebody)
+    const message=parsebody.split("=")[1]
+    fs.writeFile("message.txt", message, (err)=>{
+      if(err){
+        console.log(err)
+        
+      }
+      
+  console.log("hu")
+  res.statusCode=302
+  res.setHeader("location","/")
+  return res.end()
+
+
+
+    })
+  })
+}
+
+
+else{
+  res.setHeader("Content-Type","text/html")
+  res.write("<html>")
+  res.write("<head><title>my first pagre</title></head>")
+res.write("<body><h1>hellow dipak nodejs</h1></body>")
+res.write("</html>")
+res.end()
+
+}
+}
+
+
+module.exports=requesthandeler
