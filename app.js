4e8bbef68c0873bc74b12d780fffde7c1a325d18
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-19 16:53:09 +0530

    error

--- app.js
+++ app.js
@@ -5,26 +5,20 @@ const bodyParser=require("body-parser")
 const app=express()
 //app.use-path paths callback callbacks
 
+const adminRout=require('./routs/admin')
+const shopadmin=require('./routs/shop')
 app.use(bodyParser.urlencoded({extended:false})) //config option extended:false
 // app.use('/', (req,res,next)=>{
 //   // res.send("<h1>this is always run</h1>")
 //   next()
 // })
 
+app.use(adminRout)
+  app.use(shopadmin)
 
-app.get('/addproduct' ,(req,res,next)=>{
-  console.log("in another middlelewherer!")
-  res.send('<form action="/product" method="POST"> <input type="text" name="name" placeholder="Product Name"> <input type="text" name="size" placeholder="Product Size"> <button type="submit">Add Product</button> </form>')
-})
-//install body-parser
-app.use('/product',(req,res,next)=>{
-  console.log(req.body)
-  res.redirect("/")
-})
-app.use('/',(req,res,next)=>{
-  console.log("in  a next  middlelewherer!")
-  res.send('<h1>hellow from express</h1>')  //insted of setheader, write
 
-})  
+  app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found </h1>')
+  })
 app.listen(3000)
 
