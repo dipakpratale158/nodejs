34d8849c3fdefec27ffadac9a177ad18923372ee
dipakpratale158 <112299138+dipakpratale158@users.noreply.github.com>
2023-05-19 15:43:07 +0530

    bothinput

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 
 const express=require("express")
+const bodyParser=require("body-parser")
 
 const app=express()
+//app.use-path paths callback callbacks
 
-app.use((req,res,next)=>{
-  console.log("in a middlelewherer!")
-next() //allows the request to continew to the next middlewhereline 
-})   //use allow to add new middlewhere function. 
-//passing function as a argument 
+app.use(bodyParser.urlencoded({extended:false})) //config option extended:false
+// app.use('/', (req,res,next)=>{
+//   // res.send("<h1>this is always run</h1>")
+//   next()
+// })
 
-app.use((req,res,next)=>{
+
+app.get('/addproduct' ,(req,res,next)=>{
+  console.log("in another middlelewherer!")
+  res.send('<form action="/product" method="POST"> <input type="text" name="name" placeholder="Product Name"> <input type="text" name="size" placeholder="Product Size"> <button type="submit">Add Product</button> </form>')
+})
+//install body-parser
+app.use('/product',(req,res,next)=>{
+  console.log(req.body)
+  res.redirect("/")
+})
+app.use('/',(req,res,next)=>{
   console.log("in  a next  middlelewherer!")
   res.send('<h1>hellow from express</h1>')  //insted of setheader, write
 
